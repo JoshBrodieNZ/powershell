63af865d926b999b1ca000fb68dfd942c387c9dd
Josh Brodie <joshbrodienz@gmail.com>
2016-12-09 09:58:22 +1300

    Update powergibbon.ps1
    Added "rescope" button which lists only windows belonging to the same process as currently selected window.
    Added "show all" button which shows all windows belonging to the same process as the currently selected window.
    Rewrote window list generating code.
    Added a banner because of course.

--- powergibbon.ps1
+++ powergibbon.ps1
@@ -1,3 +1,11 @@
+$banner=@"
+  ___                     ___ _ _    _
+ | _ \_____ __ _____ _ _ / __(_) |__| |__  ___ _ _
+ |  _/ _ \ V  V / -_) '_| (_ | | '_ \ '_ \/ _ \ ' \
+ |_| \___/\_/\_/\___|_|  \___|_|_.__/_.__/\___/_||_|
+"@
+
+Write-Host $banner
 if (("APIFuncs" -as [type]) -eq $null)
 {
   #Maybe eventually turn this C# into powershell
@@ -23,6 +31,8 @@ Add-Type @"
        public static extern bool EnableWindow(IntPtr hWnd, bool bEnable);
        [DllImport("user32.dll")]
        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+			 [DllImport("user32.dll")]
+			 public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
 
        [DllImport("user32")]
        [return: MarshalAs(UnmanagedType.Bool)]
@@ -69,22 +79,43 @@ function Get-WindowName($hwnd) {
         $sb.tostring()
     }
 }
-
+$windowList = New-Object System.Collections.ArrayList
 function RefreshWindowList(){
-    $windowList = New-Object 'System.Collections.Generic.Dictionary[Int,String]'
-    foreach ($hwnd in ([APIFuncs]::GetChildWindows([APIFuncs]::GetDesktopWindow())))
+    param(
+      [Parameter(Mandatory=$false)][IntPtr]$processScope
+    )
+
+    $windowList.Clear()
+		$combobox.Items.Clear()
+    foreach ($hWnd in ([APIFuncs]::GetChildWindows([APIFuncs]::GetDesktopWindow())))
     {
+            #WindowName
             $windowName = (Get-WindowName($hwnd))
             if ($windowName -eq $null)
             {
                 $windowName = "null"
             }
-            $windowList.Add($hwnd, $windowName)
+
+						#ProcessID
+            $p = [IntPtr]::Zero
+            [void][APIFuncs]::GetWindowThreadProcessId($hWnd, [ref]$p)
+            if($processScope){
+              if(!($processScope -eq $p))
+              {
+                continue
+              }
+            }
+            #ProcessName
+						$processName = (Get-Process -ID $p).ProcessName
+						#WindowObject
+            $windowObject = New-Object PSObject -Property @{Name=$windowName;Handle=$hWnd;processId=$p;processName=$processName}
+            $windowObject | Add-Member ScriptMethod ToString {$this.Name} -force
+            [void]$windowList.Add($windowObject)
     }
-    $dataSource = New-Object System.Windows.Forms.BindingSource -args $windowList, $null
-    $combobox.DataSource =  $dataSource
-    $combobox.DisplayMember = "Value"
-    $combobox.ValueMember = "Key"
+		$combobox.Items.AddRange($windowList)
+}
+function InitialiseWindowList(){
+	RefreshWindowList
 }
 
 $windowHandle = 0
@@ -97,30 +128,42 @@ $form = New-Object Windows.Forms.Form
 $combobox = New-Object System.Windows.Forms.ComboBox
 $combobox.Location = New-Object System.Drawing.Point(25,0)
 $combobox.Size = New-Object System.Drawing.Size(250,75)
-RefreshWindowList
+InitialiseWindowList
 
 $combobox_SelectedIndexChanged =
 {
-   $windowHandle = $combobox.selectedItem.Key
-   $windowTitle = $combobox.selectedItem.Value
-   $windowLabel.Text = "$windowHandle ($windowTitle)"
+   $windowHandle = $combobox.selectedItem.Handle
+   $windowTitle = $combobox.selectedItem.Name
+	 $windowPID = $combobox.selectedItem.processId
+	 $windowProcessName = $combobox.selectedItem.processName
+
+   $windowLabel1.Text = "$windowTitle ($windowHandle)"
+	 $windowLabel2.Text = "$windowPID - $windowProcessName"
 }
 
 $combobox.add_SelectedIndexChanged($combobox_SelectedIndexChanged)
 $form.Controls.Add($combobox)
 
-#WindowName
-$windowLabel = New-Object System.Windows.Forms.Label
-$windowLabel.Text = "Window Name Goes Here"
-$windowLabel.Location = New-Object System.Drawing.Point(25,25)
-$form.Controls.Add($windowLabel)
+#Label1
+$windowLabel1 = New-Object System.Windows.Forms.Label
+$windowLabel1.Text = "Window Name Goes Here"
+$windowLabel1.Location = New-Object System.Drawing.Point(25,25)
+$windowLabel1.Size = New-Object System.Drawing.Size(250,25)
+$form.Controls.Add($windowLabel1)
+
+#Label2
+$windowLabel2 = New-Object System.Windows.Forms.Label
+$windowLabel2.Text = "Window Name Goes Here"
+$windowLabel2.Location = New-Object System.Drawing.Point(25,50)
+$windowLabel2.Size = New-Object System.Drawing.Size(250,25)
+$form.Controls.Add($windowLabel2)
 
 #EnableWindow
 $enableButton = New-Object System.Windows.Forms.Button
 $enableButton.Text = "Enable"
-$enableButton.Location = New-Object System.Drawing.Point(25,50)
+$enableButton.Location = New-Object System.Drawing.Point(25,75)
 $enableButton_click = {
-    [APIFuncs]::EnableWindow($combobox.selectedItem.Key, 1)
+    [APIFuncs]::EnableWindow($combobox.selectedItem.Handle, 1)
 }
 $enableButton.Add_Click($enableButton_click)
 $form.Controls.Add($enableButton)
@@ -128,9 +171,9 @@ $form.Controls.Add($enableButton)
 #DisableWindow
 $disableButton = New-Object System.Windows.Forms.Button
 $disableButton.Text = "Disable"
-$disableButton.Location = New-Object System.Drawing.Point(25,75)
+$disableButton.Location = New-Object System.Drawing.Point(25,100)
 $disableButton_click = {
-    [APIFuncs]::EnableWindow($combobox.selectedItem.Key, 0)
+    [APIFuncs]::EnableWindow($combobox.selectedItem.Handle, 0)
 }
 $disableButton.Add_Click($disableButton_click)
 $form.Controls.Add($disableButton)
@@ -138,9 +181,9 @@ $form.Controls.Add($disableButton)
 #ShowWindow
 $showButton = New-Object System.Windows.Forms.Button
 $showButton.Text = "Show"
-$showButton.Location = New-Object System.Drawing.Point(25,100)
+$showButton.Location = New-Object System.Drawing.Point(25,125)
 $showButton_click = {
-    [APIFuncs]::ShowWindow($combobox.selectedItem.Key, 1)
+    [APIFuncs]::ShowWindow($combobox.selectedItem.Handle, 1)
 }
 $showButton.Add_Click($showButton_click)
 $form.Controls.Add($showButton)
@@ -148,9 +191,9 @@ $form.Controls.Add($showButton)
 #HideWindow
 $hideButton = New-Object System.Windows.Forms.Button
 $hideButton.Text = "Hide"
-$hideButton.Location = New-Object System.Drawing.Point(25,125)
+$hideButton.Location = New-Object System.Drawing.Point(25,150)
 $hideButton_click = {
-    [APIFuncs]::ShowWindow($combobox.selectedItem.Key, 0)
+    [APIFuncs]::ShowWindow($combobox.selectedItem.Handle, 0)
 }
 $hideButton.Add_Click($hideButton_click)
 $form.Controls.Add($hideButton)
@@ -158,12 +201,40 @@ $form.Controls.Add($hideButton)
 #RefreshList
 $refreshButton = New-Object System.Windows.Forms.Button
 $refreshButton.Text = "Refresh List"
-$refreshButton.Location = New-Object System.Drawing.Point(25,150)
+$refreshButton.Location = New-Object System.Drawing.Point(25,175)
 $refreshButton_click = {
     RefreshWindowList
 }
 $refreshButton.Add_Click($refreshButton_click)
 $form.Controls.Add($refreshButton)
+
+#Refresh Rescope
+#TODO: Consider whether this could probably be implemented more effectively by getting the parent of the selected window
+$rescopeButton = New-Object System.Windows.Forms.Button
+$rescopeButton.Text = "Rescope"
+$rescopeButton.Location = New-Object System.Drawing.Point(25,200)
+$rescopeButton_click = {
+  RefreshWindowList($combobox.selectedItem.processId)
+}
+$rescopeButton.Add_Click($rescopeButton_click)
+$form.Controls.Add($rescopeButton)
+
+#ShowAll
+$showAllButton = New-Object System.Windows.Forms.Button
+$showAllButton.Text = "Show All"
+$showAllButton.Location = New-Object System.Drawing.Point(25,225)
+$showAllButton_click = {
+  foreach ($hWnd in ([APIFuncs]::GetChildWindows([APIFuncs]::GetDesktopWindow())))
+  {
+		$p = [IntPtr]::Zero
+		[void][APIFuncs]::GetWindowThreadProcessId($hWnd, [ref]$p)
+		if ($p -eq $combobox.selectedItem.processId){
+			[APIFuncs]::ShowWindow($hWnd, 1)
+		}
+  }
+}
+$showAllButton.Add_Click($showAllButton_click)
+$form.Controls.Add($showAllButton)
 
 $form.Text = "PowerGibbon"
 
